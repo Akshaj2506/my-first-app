a441cfbe9125d1186513b5c32cd5629e52a40f2a
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2023-07-03 14:38:12 +0530

    created nav for textutils project using bootsrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-let name = "Akshaj"
 function App() {
   return (
       <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">TextUtils</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">About</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
       </nav>
-      <div className="container">
-        <h1>Hello {name}</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit omnis fugiat libero, totam cupiditate quae, ratione consequatur culpa aliquam, placeat perspiciatis nobis? Iure aspernatur vitae voluptas commodi, autem et delectus sit natus tempora, atque eaque. Dolorem corporis iusto, tempore rem error expedita explicabo, repellat suscipit sit molestiae excepturi! Aliquid?</p>
-      </div>
       </>
     );
 }
