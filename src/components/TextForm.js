08d655d39e76094c254a881dfe5770680b288271
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2023-07-11 19:52:47 +0530

    removed extra code

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,19 +12,6 @@ export default function TextForm(props) {
    const handleChange = (event) => {
       setText(event.target.value);
    }
-   const isAlphaNumeric = (str) => {
-      var code, i, len;
-
-      for (i = 0, len = str.length; i < len; i++) {
-         code = str.charCodeAt(i);
-         if (!(code > 47 && code < 58) && // numeric (0-9)
-            !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
-            return false;
-         }
-      }
-      return true;
-   };
    const setWordCount = () => {
       const words = text.split(" ");
       words.forEach((word, i) => {
